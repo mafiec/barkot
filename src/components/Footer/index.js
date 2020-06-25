10c8fc4de2f88d974c747b40ef2c9612ade428db
Max <max@dietr.net>
2020-06-25 09:50:09 +0200

    Add changes to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,22 +3,26 @@ import './style.css'
 
 const Footer = (props) => {
     return(
-        <footer className='footer'>
-            <p className='contact'>KONTAKT</p>
-            <p>Fundacja Dzieci Etiopii Barkot</p>
-            <p>tel. 506 300 585</p>
-            <p>fundacjabarkot@gmail.com</p>
-            <div className='socialIcons'>
-                <a target='_blank' href='https://www.facebook.com/dzieci.etiopii/'>
-                    <img src={require('../../assets/fb.png')} alt='Facebook' />
-                </a>
-                <a target='_blank' href='https://www.youtube.com/channel/UCywxlkS-kdEmJh-_dK08ONw/'>
-                    <img src={require('../../assets/yt.png')} alt='Youtube' />
-                </a>
-                <a target='_blank' href='https://www.instagram.com/fundacjabarkot/'>
-                    <img src={require('../../assets/insta.png')} alt='Instagram' />
-                </a>
-            </div>
+        <footer>
+            <p id='footerTitle'>KONTAKT</p>
+              <div id='footer'>
+                <div id='footerContact'>
+                    <p>fundacjabarkot@gmail.com</p>
+                    <p id='tel'>tel. 506 300 585</p>
+                </div>
+                <div className='socialIcons'>
+                    <p>Znajdziesz nas również tutaj:</p>
+                    <a target='_blank' href='https://www.facebook.com/dzieci.etiopii/'>
+                        <img src={require('../../assets/fb.png')} alt='Facebook' />
+                    </a>
+                    <a target='_blank' href='https://www.youtube.com/channel/UCywxlkS-kdEmJh-_dK08ONw/'>
+                        <img src={require('../../assets/yt.png')} alt='Youtube' />
+                    </a>
+                    <a target='_blank' href='https://www.instagram.com/fundacjabarkot/'>
+                        <img src={require('../../assets/insta.png')} alt='Instagram' />
+                    </a>
+                </div>
+              </div>
         </footer>
     );
 }
