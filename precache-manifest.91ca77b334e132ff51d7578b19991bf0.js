f7cc5a75abb2ad13b1b211bf3e404e2bc57fee57
Max <max@dietr.net>
2020-06-26 16:31:03 +0200

    Updates

---
+++ precache-manifest.91ca77b334e132ff51d7578b19991bf0.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ef478da60dc3026ac3879171a629aa",
+    "url": "/barkot/index.html"
+  },
+  {
+    "revision": "5444949087142fd9fe9d",
+    "url": "/barkot/static/css/main.1943afd9.chunk.css"
+  },
+  {
+    "revision": "3955471b4f7b71710092",
+    "url": "/barkot/static/js/2.5209d4a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/barkot/static/js/2.5209d4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5444949087142fd9fe9d",
+    "url": "/barkot/static/js/main.ce8a26dd.chunk.js"
+  },
+  {
+    "revision": "02f604e126dbd53e9119",
+    "url": "/barkot/static/js/runtime-main.51b41a00.js"
+  },
+  {
+    "revision": "d8e9b704f92e5b50670f008cc57f9c93",
+    "url": "/barkot/static/media/abraham0.d8e9b704.jpg"
+  },
+  {
+    "revision": "048bb42e3822d107aae9be9b19cbc6f1",
+    "url": "/barkot/static/media/binyam0.048bb42e.jpg"
+  },
+  {
+    "revision": "a34115c2cdf5e82809d47de4d006f95a",
+    "url": "/barkot/static/media/binyam1.a34115c2.jpg"
+  },
+  {
+    "revision": "105f17630a4ff11b6043dd7bfe5a33ea",
+    "url": "/barkot/static/media/binyam2.105f1763.jpg"
+  },
+  {
+    "revision": "bebfcf95e9ac14ca59bc7e5dc64e4540",
+    "url": "/barkot/static/media/binyam3.bebfcf95.jpg"
+  },
+  {
+    "revision": "e717253c0312fe7d170a4cf716de2ec5",
+    "url": "/barkot/static/media/binyam4.e717253c.jpg"
+  },
+  {
+    "revision": "2541cc282e0dfa8d416ab91b1e34b8fa",
+    "url": "/barkot/static/media/cork-board.2541cc28.png"
+  },
+  {
+    "revision": "3cd2ddefccc1e8f87d9c2e85ba636b15",
+    "url": "/barkot/static/media/erdachew0.3cd2ddef.jpg"
+  },
+  {
+    "revision": "0563aa41c4ede75fb44da5103528ad12",
+    "url": "/barkot/static/media/estifanos0.0563aa41.jpg"
+  },
+  {
+    "revision": "4b0fb5fb39014115325c900b90c50562",
+    "url": "/barkot/static/media/estifanos1.4b0fb5fb.jpg"
+  },
+  {
+    "revision": "c89af7fe0bde81c1153b2b32d8891cc9",
+    "url": "/barkot/static/media/habtamu0.c89af7fe.jpg"
+  },
+  {
+    "revision": "5e5f271bc40b0f3d82e251aad5cf3e56",
+    "url": "/barkot/static/media/habtamu1.5e5f271b.jpg"
+  },
+  {
+    "revision": "5eda50757fa201c86f1b3e6e19362b98",
+    "url": "/barkot/static/media/habtamu2.5eda5075.jpg"
+  },
+  {
+    "revision": "0f5d25586893491722b49c5d5f155401",
+    "url": "/barkot/static/media/header.0f5d2558.jpg"
+  },
+  {
+    "revision": "c9931e8e6da751578c510ac2e03e9f17",
+    "url": "/barkot/static/media/logo.c9931e8e.png"
+  },
+  {
+    "revision": "e6d451f52e81baff0c06e4797c211569",
+    "url": "/barkot/static/media/naszaMisja2.e6d451f5.jpg"
+  },
+  {
+    "revision": "65df756e90dc529821d9e947cc89caa2",
+    "url": "/barkot/static/media/naszaMisja3.65df756e.jpg"
+  },
+  {
+    "revision": "571c9a956ab72e2170bfa304e9d4fa3c",
+    "url": "/barkot/static/media/oNas1.571c9a95.jpg"
+  },
+  {
+    "revision": "1dfa8a866b94a13a981bc6c60e73a96a",
+    "url": "/barkot/static/media/projektOrientacyjny.1dfa8a86.jpg"
+  },
+  {
+    "revision": "050d7ece07567704f09f688bbdcc98c8",
+    "url": "/barkot/static/media/projektRehabi.050d7ece.jpg"
+  },
+  {
+    "revision": "dfd6d004484b47ac5d5aa9707d6479e0",
+    "url": "/barkot/static/media/projektReintegracja.dfd6d004.jpg"
+  },
+  {
+    "revision": "327261c3b33c0e22d85fddf450cc060a",
+    "url": "/barkot/static/media/projektSwietlica.327261c3.jpg"
+  },
+  {
+    "revision": "bfa5dba0d4e4e67c881b32f7d7a673b8",
+    "url": "/barkot/static/media/przekazDatek1.bfa5dba0.jpg"
+  },
+  {
+    "revision": "102335548db141c9fad46343bad31aee",
+    "url": "/barkot/static/media/raporty.10233554.jpg"
+  },
+  {
+    "revision": "039240da19beda9a389437fcd19667d1",
+    "url": "/barkot/static/media/solomon0.039240da.jpg"
+  },
+  {
+    "revision": "bcac6a0c862386efa6e20753e1454870",
+    "url": "/barkot/static/media/solomon1.bcac6a0c.jpg"
+  },
+  {
+    "revision": "4571dddcf55e333234b7a85ce8aa2f76",
+    "url": "/barkot/static/media/solomon2.4571dddc.jpg"
+  },
+  {
+    "revision": "20aec4165768ce8fbf5702841dd9a040",
+    "url": "/barkot/static/media/solomon3.20aec416.jpg"
+  },
+  {
+    "revision": "4acf24464c5d3ed3b43833649d5440e9",
+    "url": "/barkot/static/media/start.4acf2446.jpg"
+  },
+  {
+    "revision": "0721b0efd43826396767acb21cb38c47",
+    "url": "/barkot/static/media/tamru0.0721b0ef.jpg"
+  },
+  {
+    "revision": "1f86933413d0f58f5cc302f2ba530370",
+    "url": "/barkot/static/media/tamru1.1f869334.jpg"
+  },
+  {
+    "revision": "4270d247d56b234176a03dae9072a304",
+    "url": "/barkot/static/media/tamru2.4270d247.jpg"
+  },
+  {
+    "revision": "bf8b876f5e759d3ffb72c4430c4a666f",
+    "url": "/barkot/static/media/tarekegn0.bf8b876f.jpg"
+  },
+  {
+    "revision": "4ac4654cb1185757448ed60a01088bfe",
+    "url": "/barkot/static/media/tarekegn1.4ac4654c.jpg"
+  },
+  {
+    "revision": "c2ad4d478058011b6ebbdbe949e1ecf1",
+    "url": "/barkot/static/media/tarekegn2.c2ad4d47.jpg"
+  },
+  {
+    "revision": "f33ccde6f931ef53ca4d008ccebec8c7",
+    "url": "/barkot/static/media/wolontariat1.f33ccde6.jpg"
+  }
+]);
